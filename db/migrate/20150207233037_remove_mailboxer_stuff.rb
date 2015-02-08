ebd3a0a005d6ae9b7d786a801dc738a0c10b8eb1
mzucco <odachi65@hotmail.com>
2015-02-08 00:40:45 -0400

    Removed a file

--- db/migrate/20150207233037_remove_mailboxer_stuff.rb
+++
@@ -1,8 +0,0 @@
-class RemoveMailboxerStuff < ActiveRecord::Migration
-  def change
-  	drop_table :mailboxer_conversation_opt_outs
-  	drop_table :mailboxer_conversations
-  	drop_table :mailboxer_notifications
-  	drop_table :mailboxer_receipts
-  end
-end
