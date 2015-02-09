ecc8c5861ff395ef1bddd716bb352ee00960aa03
mzucco <odachi65@hotmail.com>
2015-02-09 06:51:55 -0400

    Changed Environment Settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,5 @@ DuelOfChampions::Application.configure do
   config.assets.debug = true
   
   config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.default_url_options = { :host => 'localhost'}
 end
